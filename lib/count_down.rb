7e1a05440db85d06f733fdae098f326b9a1bdfb3
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-01 16:25:40 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,15 +1,9 @@
-# counter = 10 
-# while counter > 0 do 
-#   puts counter
-#   counter -= 1 
-#   if counter == 0 
-#     puts "Happy New Year!"
-#   end
-# end 
+counter = 10 
+while counter > 0 do 
+  puts counter
+  counter -= 1 
+  if counter == 0 
+    puts "Happy New Year!"
+  end
+end 
 
-count = 10
-while count >= 1 do
-  puts count
-  count -= 1
-end
-puts "Happy New Year!"
